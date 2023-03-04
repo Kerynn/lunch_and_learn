a4d38bcbf262906e495a56e099ac447ec58bd621
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-03 17:22:09 -0800

    create country facade for one random country

---
+++ spec/facades/country_facade_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe CountryFacade do 
+  before :each do 
+    stub_request(:get, "https://restcountries.com/v3.1/all")
+      .to_return(status: 200, body: File.read('./spec/fixtures/all_countries_response.json'), headers: {})    
+  end
+
+  it 'can return the collection of country result objects' do 
+    countries = CountryFacade.get_all_countries
+
+    expect(countries).to be_an Array
+    countries.each do |country|
+      expect(country).to be_an_instance_of(Country)
+    end
+  end
+
+  it 'can return one random country object' do 
+    random_country = CountryFacade.random_country
+
+    expect(random_country).to be_an_instance_of(Country)
+  end
+end
