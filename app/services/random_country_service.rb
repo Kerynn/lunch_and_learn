d7bd2095e96e20f5557e3b96445bc6a15634ad00
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-03 17:02:30 -0800

    create random country service

---
+++ app/services/random_country_service.rb
@@ -0,0 +1,14 @@
+class RandomCountryService
+
+  def self.all_countries_search
+    parse(conn.get('/v3.1/all'))
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: 'https://restcountries.com')
+  end
+end
