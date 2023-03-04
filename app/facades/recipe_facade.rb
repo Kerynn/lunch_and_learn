f1f939a528d86d4a0473009f25f35c6d2e677465
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-03 19:35:09 -0800

    add country name to recipe creation in recipe facade

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -3,7 +3,7 @@ class RecipeFacade
   def self.get_all_recipes(country_name)
     recipes = RecipeService.recipe_search(country_name)
     recipes[:hits].map do |recipe_data|
-      Recipe.new(recipe_data)
+      Recipe.new(recipe_data, country_name)
     end
   end
 end
