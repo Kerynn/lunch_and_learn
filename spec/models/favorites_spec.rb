76cc42f9d5f4731ce1453fb903e97aeff8466e7e
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-05 09:49:24 -0800

    add validates for favorites

--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Favorite do 
+  describe 'validations' do 
+    it { should validate_presence_of :country }
+    it { should validate_presence_of :recipe_link }
+    it { should validate_presence_of :recipe_title }
+  end
+
   describe 'relationships' do 
     it { should belong_to :user }
   end
