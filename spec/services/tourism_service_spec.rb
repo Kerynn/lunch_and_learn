0d3b552b19bd5b5cb961f4d1734638327cc0ec82
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-06 10:11:03 -0800

    add fixtures for tourism specs to improve speed of tests

--- spec/services/tourism_service_spec.rb
+++ spec/services/tourism_service_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe CountryService do 
   it 'can return the country tourism info' do 
-    WebMock.allow_net_connect!
+    stub_request(:get, "https://api.geoapify.com/v2/places?categories=tourism.sights&filter=circle:2.33,48.87,20000&limit=20&apiKey=#{ENV['places_api_key']}")
+      .to_return(status: 200, body: File.read('./spec/fixtures/final_fixtures/tourism_response.json'), headers: {}) 
 
     response = TourismService.tourism_search(2.33, 48.87)
 
