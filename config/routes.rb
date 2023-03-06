b0f72beee24b746eb8ca9ee9621613e9500e216c
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-06 13:43:49 -0800

    add favorite index route with serializer for a user favorites

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :recipes, only: :index
       resources :learning_resources, only: :index
       resources :users, only: :create
-      resources :favorites, only: :create
+      resources :favorites, only: [:index, :create]
       resources :tourist_sights, only: :index
     end
   end
