ccb405a2adb22c5eccdf042bae651c91acf8b5be
Kerynn Davis <kerynn.davis@gmail.com>
2023-03-06 08:46:46 -0800

    create tourism service

---
+++ app/services/tourism_service.rb
@@ -0,0 +1,18 @@
+class TourismService
+
+  def self.tourism_search(longitude, latitude)
+    parse(conn.get("/v2/places?filter=circle:#{longitude},#{latitude},30000"))
+  end
+
+  def self.parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: 'https://api.geoapify.com') do |f|
+      f.params['categories'] = 'tourism.sights'
+      f.params['limit'] = 20
+      f.params['apiKey'] = ENV['places_api_key']
+    end
+  end
+end
---
+++ spec/services/tourism_service_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe CountryService do 
+  it 'can return the country tourism info' do 
+    WebMock.allow_net_connect!
+
+    response = TourismService.tourism_search(2.33, 48.87)
+
+    expect(response).to be_a Hash
+    expect(response).to have_key(:type)
+    expect(response).to have_key(:features)
+    expect(response[:features]).to be_an Array
+
+    response[:features].each do |result|
+      expect(result).to have_key(:type)
+      expect(result).to have_key(:properties)
+      expect(result[:properties]).to be_a Hash
+      expect(result[:properties]).to have_key(:name)
+      expect(result[:properties]).to have_key(:country)
+      expect(result[:properties]).to have_key(:formatted)
+      expect(result[:properties]).to have_key(:categories)
+      expect(result[:properties]).to have_key(:place_id)
+    end
+  end 
+end 
